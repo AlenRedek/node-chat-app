f5daa8a8103a23ecba54f8d33eae1ace7de009ad
Alen Redek <alen@redek.me>
2017-06-22 13:21:48 +0200

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -17,15 +17,24 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // Emits new event to a single connection
+  // socket.emit('newMessage', {
+  //   from: 'server@redek.me',
+  //   text: 'Server welcomes you',
+  //   createdAt: 'right now'
+  // });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    // Emits new event to every single connection
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
-  socket.emit('newMessage', {
-    from: 'server@redek.me',
-    text: 'Server welcomes you',
-    createdAt: 'right now'
-  });
+
 
   socket.on('disconnect', () => {
     console.log('User was disconnected from server');
